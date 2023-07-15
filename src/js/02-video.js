fdcf0e6d8f24a2a3207d7aa65aca0b3e75b20847
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-16 00:08:15 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector("#vimeo-player");
+const player = new Player(iframe);
+
+const onTime = function (data) { 
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime)
+    .then(function (seconds) {
+        console.log(`Saved currentTime is ${seconds} seconds`);
+    }).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+        console.log(`currentTime error`);
+            break;
+
+        default:
+        console.log(`This is default`);
+            break;
+    }
+});
+player.on("timeupdate", throttle(onTime, 1000));
