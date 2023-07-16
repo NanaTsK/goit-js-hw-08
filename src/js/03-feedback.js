359c257e23a4518b9cdd9cac814ebc9960165afa
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-16 10:53:05 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+
+
+const loginForm = document.querySelector(".js-feedback-form");
+loginForm.addEventListener("submit", handleSubmit);
+
+const textArea = document.querySelector(".js-feedback-form textarea");
+textArea.addEventListener("input", onTextAreaInput)
+
+
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+        const {elements: {email, message}} = event.currentTarget;
+
+        const loginObj = {
+        email: email.value,
+        message: message.value,
+    }
+    console.log(loginObj);
+    event.currentTarget.reset();
+};
+
+function onTextAreaInput(event) { 
+
+    const massage = event.currentTarget.value;
+    
+    localStorage.setItem("feedback-form-state", massage)
+
+    console.log(massage);
+};
 
