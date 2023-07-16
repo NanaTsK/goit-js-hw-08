e351cabcdfd6731c1e189a977c18a26db31ce5e0
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-16 14:35:46 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,18 +4,20 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector("#vimeo-player");
 const player = new Player(iframe);
 
+const CURRENT_TIME_KEY = "videoplayer-current-time";
+
 const onTime = function (data) { 
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+    localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(data.seconds));
 };
 
-const currentTime = localStorage.getItem('videoplayer-current-time');
+const currentTime = localStorage.getItem(CURRENT_TIME_KEY);
 
 player.setCurrentTime(currentTime)
     .then(function (seconds) {
         console.log(`Saved currentTime is ${seconds} seconds`);
     }).catch(function (error) {
     switch (error.name) {
-        case 'RangeError':
+        case "RangeError":
         console.log(`currentTime error`);
             break;
 
